c43c5f57364633c9f15677ee39c0d59ceb8a9a6e
chgad <chgad.games@gmail.com>
2020-01-30 10:10:13 +0100

    Add missing apps to settings. Add dummy test. Change pytest config.

--- config/settings/common.py
+++ config/settings/common.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "quiz.apps.QuizConfig",
+    "glossary.apps.GlossaryConfig",
+    "content.apps.ContentConfig"
 ]
 
 MIDDLEWARE = [
