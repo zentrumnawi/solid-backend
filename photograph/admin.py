a01e05141da0efbce05dd2df985254d98b1f376f
suehtomit <kozlowski@itp.uni-frankfurt.de>
2020-06-17 15:37:27 +0200

    Implemented PhotographForm into PhotographAdmin

--- photograph/admin.py
+++ photograph/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import Photograph
+from .forms import PhotographForm
 
 
 class PhotographAdmin(admin.ModelAdmin):
-    list_display = ["id", "img", "author"]
+    form = PhotographForm
+    fields = [
+        "img",
+        ("length_value", "length_unit", "pixel_number"),
+        "img_alt",
+        "description",
+        "date",
+        "author",
+        "license",
+    ]
+    list_display = ["id", "img", "img_original_scale", "author"]
 
 
 admin.site.register(Photograph, PhotographAdmin)
