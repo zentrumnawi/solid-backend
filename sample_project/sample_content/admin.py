694fa05b8d535b74489d5d323a082b1efbe2a752
christian <chgad.games@gmail.com>
2020-07-07 15:46:52 +0200

    Add sample_content to settings, create Demo Profile model inheriting the BaseModel.

--- sample_project/sample_content/admin.py
+++ sample_project/sample_content/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import MyProfile
+from solid_backend.photograph.admin import PhotographInline
 # Register your models here.
+
+class MyProfileAdmin(admin.ModelAdmin):
+    inlines = [
+        PhotographInline,
+    ]
+
+
+admin.site.register(MyProfile, MyProfileAdmin)
--- sample_project/sample_content/models.py
+++ sample_project/sample_content/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from solid_backend.content.models import BaseProfile
 # Create your models here.
+
+
+class MyProfile(BaseProfile):
+    name = models.CharField(max_length=200, default="")
+
