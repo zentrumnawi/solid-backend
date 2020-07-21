03057c512abd0e5a5b5e7e1c948c6b8a98c8a8ad
Timotheus Kozlowski <timotheus.kozlowski@gmx.de>
2020-07-21 18:03:59 +0200

    Adjusted queryset for altered valid_to field

--- solid_backend/message/views.py
+++ solid_backend/message/views.py
@@ -1,5 +1,6 @@
 from datetime import date
 
+from django.db.models import Q
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .models import Message
@@ -8,11 +9,12 @@ from .serializers import MessageSerializer
 
 class MessageEndpoint(ReadOnlyModelViewSet):
     """
-    Endpoint that provides the database table of currently valid messages.
+    Provide database table of currently valid messages.
     """
 
     queryset = Message.objects.filter(
-        valid_from__lte=date.today(), valid_to__gte=date.today()
+        Q(valid_from__lte=date.today(), valid_to__gte=date.today())
+        | Q(valid_from__lte=date.today(), valid_to__isnull=True)
     )
     serializer_class = MessageSerializer
     name = "message"
