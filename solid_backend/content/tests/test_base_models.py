13e9fabd8894a0b3daa7d5b788daedd19155a1c6
Timotheus Kozlowski <timotheus.kozlowski@gmx.de>
2021-02-15 10:45:32 +0100

    Create TestBaseProfileModelExists

--- solid_backend/content/tests/test_base_models.py
+++ solid_backend/content/tests/test_base_models.py
@@ -2,6 +2,20 @@ from django.db import models
 from mptt.models import TreeForeignKey
 
 
+class TestBaseProfileModelExists:
+    """
+    Test whether an object BaseProfile can be imported and is a Django model.
+    """
+
+    def test_model_exists(self):
+        pass
+
+    def test_model_is_django_model(self):
+        from solid_backend.content.models import BaseProfile
+
+        assert issubclass(BaseProfile, models.Model)
+
+
 class TestTreeNodeModelExists:
     """
     Test whether an object TreeNode can be imported and is a Django model.
