41aff6bc4e2b8cfdf81dd1f7d9f02d888173cd37
Timotheus Kozlowski <timotheus.kozlowski@gmx.de>
2021-02-16 10:17:57 +0100

    Create ProfileEndpoint.get_queryset()

--- solid_backend/content/views.py
+++ solid_backend/content/views.py
@@ -1,5 +1,8 @@
 from django.conf import settings
+from django.db.models import Prefetch
 from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from solid_backend.photograph.models import Photograph
 
 from .models import TreeNode
 from .serializers import TreeNodeSerializer
@@ -10,6 +13,28 @@ class ProfileEndpoint(ReadOnlyModelViewSet):
     Endpoint that provides the database table of the tree structure of all profiles with their related photographs
     """
 
-    queryset = TreeNode.objects.root_nodes()
     serializer_class = TreeNodeSerializer
     name = "profile"
+
+    def get_queryset(self):
+        if not hasattr(TreeNode, "profiles"):
+            return TreeNode.objects.root_nodes()
+
+        # Get the deepest level of the tree structure.
+        level_max = 0
+
+        for obj in TreeNode.objects.only("level"):
+            if level_max < obj.level:
+                level_max = obj.level
+
+        # Generate a list of lookups for the profiles with their related photographs
+        # ordered by their profile_positions up to level_max in the tree structure.
+        lookup = "profiles__photographs"
+        queryset = Photograph.objects.order_by("profile_position")
+        lookup_list = [Prefetch(lookup, queryset=queryset)]
+
+        for i in range(level_max):
+            lookup = "children__" + lookup
+            lookup_list.append(Prefetch(lookup, queryset=queryset))
+
+        return TreeNode.objects.root_nodes().prefetch_related(*lookup_list)
