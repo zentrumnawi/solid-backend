0259eb1345497b132435c1c8b3d76cf89457d2fd
christian <chgad.games@gmail.com>
2021-05-05 18:46:34 +0200

    Remove incorrect serializer variable reference. Substitute with data variable.

--- solid_backend/contact/views.py
+++ solid_backend/contact/views.py
@@ -18,10 +18,10 @@ class ContactView(generics.GenericAPIView):
         message = send_mail(
             subject=data["subject"],
             message=data["message"],
-            from_email="{0} <{1}>".format(serializer["name"], data["email"]),
+            from_email="{0} <{1}>".format(data["name"], data["email"]),
             recipient_list=[settings.SYSTEM_EMAIL],
             fail_silently=False,
         )
         if not message:
             return response.Response(status=status.HTTP_400_BAD_REQUEST)
-        return response.Response(data=serializer)
+        return response.Response(data=data)
