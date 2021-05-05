a9b00d3724c82d0e7a70adadcad98cc4fce98d47
christian <chgad.games@gmail.com>
2021-05-05 20:59:53 +0200

    Change Email sending.

--- solid_backend/contact/views.py
+++ solid_backend/contact/views.py
@@ -1,8 +1,24 @@
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from rest_framework import generics, response, status
 
 from .serializers import ContactSerializer
+
+
+def format_message(url, name, email, message):
+    text = (
+        f"Name: {name}",
+        f"E-Mail: {email}",
+        "Nachricht:",
+        "",
+        message,
+        "",
+        "---------",
+        "",
+        f"Das ist eine automatisch generierte Nachricht (System URL: {url})",
+    )
+
+    return "\n".join(text)
 
 
 class ContactView(generics.GenericAPIView):
@@ -15,13 +31,21 @@ class ContactView(generics.GenericAPIView):
         serializer.is_valid(raise_exception=True)
         data = serializer.data
 
-        message = send_mail(
+        message = EmailMessage(
             subject=data["subject"],
-            message=data["message"],
-            from_email="{0} <{1}>".format(data["name"], data["email"]),
-            recipient_list=[settings.SYSTEM_EMAIL],
-            fail_silently=False,
+            body=format_message(
+                request.build_absolute_uri(),
+                data["name"],
+                data["email"],
+                data["message"],
+            ),
+            from_email="Feedback <{}>".format(settings.SYSTEM_EMAIL),
+            to=[settings.SYSTEM_EMAIL],
+            reply_to=[data["email"], settings.SYSTEM_EMAIL]
         )
+        message.send(fail_silently=False)
+
         if not message:
             return response.Response(status=status.HTTP_400_BAD_REQUEST)
+
         return response.Response(data=data)
