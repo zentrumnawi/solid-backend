2ca925ce7b505566349f7e2cd971a8a2546277c4
christian <chgad.games@gmail.com>
2022-02-01 19:36:03 +0100

    Add CategoryImageField to access the tumbnail of the first tagged SlideShow for each Tag.

--- solid_backend/slideshow/serializers.py
+++ solid_backend/slideshow/serializers.py
@@ -1,6 +1,8 @@
+from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 from taggit.models import Tag
 from taggit.serializers import TagListSerializerField
+
 from solid_backend.photograph.serializers import PhotographSerializer
 from solid_backend.utils.serializers import DynamicExcludeModelSerializer
 
@@ -33,7 +35,30 @@ class SlideshowSerializer(serializers.ModelSerializer):
         exclude = ["active"]
 
 
+class CategoryImageField(serializers.FileField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs["source"] = "*"
+        super(CategoryImageField, self).__init__(*args, **kwargs)
+
+    def to_representation(self, value):
+        """
+        Retrieve the thumbnail url of the first SlideShow associated with a tag.
+        :param value:
+        :return:
+        """
+        # Since Tags are associated with a SlideShow via
+        # a through model TaggedItem which itself has a
+        # GenericRelation to the SlideShow things get complicated.
+        first_tagged_item = value.taggit_taggeditem_items.first()
+        # Get SlideShow ContentType object
+        content_type = ContentType.objects.get(id=first_tagged_item.content_type_id)
+        image = content_type.get_object_for_this_type(id=first_tagged_item.object_id).title_image.img
+        return super(CategoryImageField, self).to_representation(image.thumbnail)
+
+
 class CategorySerializer(serializers.ModelSerializer):
+    image = CategoryImageField()
 
     class Meta:
         model = Tag
