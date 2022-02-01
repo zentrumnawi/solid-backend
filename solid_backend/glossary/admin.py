a229b1458de4475bd25e6725743cf693500e7d28
christian <chgad.games@gmail.com>
2022-02-01 15:07:57 +0100

    Add GlossaryADminForm to validate that atleast either text OR links are provided.

--- solid_backend/glossary/admin.py
+++ solid_backend/glossary/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
+from .forms import GlossaryEntryAdminForm
 from .models import GlossaryEntry
 
 
 class GlossaryEntryAdmin(admin.ModelAdmin):
+    form = GlossaryEntryAdminForm
     list_display = ["id", "term"]
 
 
