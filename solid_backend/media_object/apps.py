a695021ba3b2a773d472c5474bad4051b6eda76c
Lisakarolina <w.hello@web.de>
2025-03-16 18:32:31 +0100

    Reformat changed fields

--- solid_backend/media_object/apps.py
+++ solid_backend/media_object/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MediaObjectConfig(AppConfig):
-    default_auto_field = 'django.db.models.AutoField'
+    default_auto_field = "django.db.models.AutoField"
     name = "solid_backend.media_object"
--- solid_backend/slideshow/apps.py
+++ solid_backend/slideshow/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SlideshowConfig(AppConfig):
-    default_auto_field = 'django.db.models.AutoField'
+    default_auto_field = "django.db.models.AutoField"
     name = "solid_backend.slideshow"
