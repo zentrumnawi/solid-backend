203dfbe0df6a78db414d37652eefd74c504f4148
Lisakarolina <w.hello@web.de>
2025-05-19 15:00:11 +0200

    Add search for tree nodes and profiles

--- solid_backend/content/views.py
+++ solid_backend/content/views.py
@@ -1,5 +1,11 @@
 from django.db.models import Prefetch
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet, GenericViewSet
+from django.db.models import Q
+from django.conf import settings
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .serializers import SERIALIZERS
+from mptt.fields import TreeForeignKey
 
 from solid_backend.media_object.models import MediaObject
 
@@ -37,3 +43,105 @@ class ProfileEndpoint(ReadOnlyModelViewSet):
             lookup_list.append(Prefetch(lookup, queryset=queryset))
 
         return TreeNode.objects.root_nodes().prefetch_related(*lookup_list)
+
+
+class SearchProfileEndpoint(ReadOnlyModelViewSet):
+    """
+    Endpoint that returns tree nodes that contain the search term in their general_information__name or general_information__synonyms fields.
+    """
+
+    serializer_class = LeavesWithProfilesSerializer
+    name = "search-profile"
+
+    def get_queryset(self):
+        search_term = self.request.query_params.get("q", "")
+        if not search_term:
+            return TreeNode.objects.none()
+
+        if SERIALIZERS:
+            for profile_type in SERIALIZERS:
+
+                model_class = SERIALIZERS[profile_type].Meta.model
+                # logger.debug(f"Model class: {model_class}")
+                # Get the related query name from the model's TreeNode field
+                for field in model_class._meta.fields:
+                    if (
+                        isinstance(field, TreeForeignKey)
+                        and field.remote_field.model == TreeNode
+                    ):
+                        related_query_name = field.related_query_name()
+                        # logger.debug(f"Related query name: {related_query_name}")
+
+                return TreeNode.objects.filter(
+                    Q(
+                        **{
+                            f"{related_query_name}__general_information__name__icontains": search_term
+                        }
+                    )
+                    | Q(
+                        **{
+                            f"{related_query_name}__general_information__synonyms__icontains": search_term
+                        }
+                    )
+                ).distinct()
+        return TreeNode.objects.none()
+
+    @action(detail=False, methods=["get"])
+    def search(self, request):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class GlobalSearchEndpoint(GenericViewSet):
+    """
+    Endpoint that searches across all content types and returns their native serialized data.
+    """
+
+    name = "global-search"
+
+    def get_queryset(self):
+        search_term = self.request.query_params.get("q", "")
+        if not search_term:
+            return []
+
+        results = []
+        if SERIALIZERS:
+            for profile_type in SERIALIZERS:
+                model = SERIALIZERS[profile_type].Meta.model
+                profile_results = model.objects.filter(
+                    # Q(general_information__sub_name__icontains=search_term) |
+                    Q(general_information__name__icontains=search_term)
+                )
+                if profile_results.exists():
+                    results.extend(profile_results)
+
+        return results
+
+    def get_serializer_class(self):
+        return SERIALIZERS.get(self.related_name, None)
+
+    def get_serializer_for_model(self, model_name):
+        for serializer_name, serializer_class in SERIALIZERS.items():
+            if serializer_class.Meta.model.__name__.lower() == model_name.lower():
+                # logger.debug(f"Serializer class: {serializer_class}")
+                return serializer_class
+        return None
+
+    @action(detail=False, methods=["get"])
+    def search(self, request):
+        queryset = self.get_queryset()
+        # Group results by type and serialize accordingly
+        # logger.debug(f"Queryset: {[item for item in queryset]}")
+        response_data = {}
+        for item in queryset:
+            model_name = item._meta.model_name
+            # logger.debug(f"Model name: {model_name}")
+            if model_name not in response_data:
+                response_data[model_name] = []
+            serializer_class = self.get_serializer_for_model(model_name)
+            if serializer_class:
+                serializer = serializer_class(item)
+                response_data[model_name].append(serializer.data)
+
+        return Response(response_data)
