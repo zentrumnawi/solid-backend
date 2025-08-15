01641e33f1627a582f6afb5b4f871dc6ad86e369
Lisakarolina <w.hello@web.de>
2025-08-15 10:40:17 +0200

    Return glossary entries grouped by tag

--- solid_backend/glossary/views.py
+++ solid_backend/glossary/views.py
@@ -3,6 +3,17 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .models import GlossaryEntry
 from .serializers import GlossaryEntrySerializer
+import logging
+from collections import defaultdict
+from rest_framework.response import Response
+
+logger = logging.getLogger(__name__)
+
+# def get_glossary_entries_by_tag(tag):
+#     """
+#     Get all glossary entries for a given tag.
+#     """
+#     return GlossaryEntry.objects.filter(tags=tag)
 
 
 class GlossaryEntryEndpoint(ReadOnlyModelViewSet):
@@ -13,5 +24,18 @@ class GlossaryEntryEndpoint(ReadOnlyModelViewSet):
     queryset = GlossaryEntry.objects.all().prefetch_related(
         Prefetch("links", queryset=GlossaryEntry.objects.order_by("term"))
     )
+
+    def list(self, request):
+        response_data = defaultdict(list)
+        queryset = self.queryset
+        
+        serialized = GlossaryEntrySerializer(
+            queryset, many=True, context={"request": request}
+        ).data
+        for item in serialized:
+            response_data[item["tags"]].append(item)
+        return Response(response_data)
+
+
     serializer_class = GlossaryEntrySerializer
     name = "glossaryentry"
