d45ada5492e111697d5055d886d78d3c63a14247
Lisakarolina <w.hello@web.de>
2025-08-15 10:40:55 +0200

    Add database field

--- solid_backend/glossary/models.py
+++ solid_backend/glossary/models.py
@@ -9,6 +9,7 @@ class GlossaryEntry(models.Model):
     term = models.CharField(max_length=100)
     text = models.TextField(null=True, blank=True, verbose_name="text (Markdown)")
     links = models.ManyToManyField("self", symmetrical=False, blank=True, null=True)
+    tags = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.term
