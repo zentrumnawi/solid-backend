8458d667e4b66b7623047c61b3cb7b1f32fe8fda
Lisakarolina <w.hello@web.de>
2025-08-29 02:08:17 +0200

    Use queryset method instead of queryset attribute

--- solid_backend/glossary/views.py
+++ solid_backend/glossary/views.py
@@ -9,33 +9,23 @@ from rest_framework.response import Response
 
 logger = logging.getLogger(__name__)
 
-# def get_glossary_entries_by_tag(tag):
-#     """
-#     Get all glossary entries for a given tag.
-#     """
-#     return GlossaryEntry.objects.filter(tags=tag)
-
 
 class GlossaryEntryEndpoint(ReadOnlyModelViewSet):
-    """
-    Endpoint that provides the database table of all glossary entries.
-    """
-
-    queryset = GlossaryEntry.objects.all().prefetch_related(
-        Prefetch("links", queryset=GlossaryEntry.objects.order_by("term"))
-    )
+    def get_queryset(self):
+        return GlossaryEntry.objects.all().prefetch_related(
+            Prefetch("links", queryset=GlossaryEntry.objects.order_by("term"))
+        )
 
     def list(self, request):
         response_data = defaultdict(list)
-        queryset = self.queryset
-        
+        queryset = self.get_queryset()
+
         serialized = GlossaryEntrySerializer(
             queryset, many=True, context={"request": request}
         ).data
         for item in serialized:
             response_data[item["tags"]].append(item)
         return Response(response_data)
-
 
     serializer_class = GlossaryEntrySerializer
     name = "glossaryentry"
